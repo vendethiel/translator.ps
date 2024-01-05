50e336cee345adfa61917a83d299c9244b1f5d77
vendethiel <vendethiel@hotmail.fr>
2024-01-05 18:41:58 +0100

    undumb

--- src/Util/Either.purs
+++ src/Util/Either.purs
@@ -1,15 +1,13 @@
 module Util.Either (fromEitherMaybe, mapL) where
 
-import Prelude (identity)
+import Prelude
 
 import Data.Bifunctor (bimap)
-import Data.Either (Either(..))
-import Data.Maybe (Maybe(..))
+import Data.Either (Either, note)
+import Data.Maybe (Maybe)
 
 mapL :: forall a b c. (a -> b) -> Either a c -> Either b c
 mapL f = bimap f identity
 
 fromEitherMaybe :: forall l r. l -> Either l (Maybe r) -> Either l r
-fromEitherMaybe _ (Right (Just x)) = Right x
-fromEitherMaybe l (Right Nothing) = Left l
-fromEitherMaybe _ (Left e) = Left e
+fromEitherMaybe = (join <<< _) <<< map <<< note
